2a64255334c4de8e44f2a0553a262453ed1079e4
Mary Khalatyan <khalatyaaan@gmail.com>
2023-01-07 16:17:14 +0800

    some fixes

--- other_tasks/third.py
+++ other_tasks/third.py
@@ -3,14 +3,15 @@
 @classmethod
 # лучшк добавить декортатор @transaction.atomic
 def buy(cls, user, item_id):
+    # лучше сделать поиск сразу с использованием .first().
+    # product = Product.objects.filter(item_id=item_id).first()
     product_qs = Product.objects.filter(item_id=item_id)
 
-    # filter возвращает queryset или None, поэтому, можно проверку переписать
-    # if product_qs:
+    # first() возвращает None или эл-т, поэтому проверку можно проести
+    # if product:
     if product_qs.exists():
-        # первый элемент можно получить через first()
-        # product = product_qs.first()
         product = product_qs[0]
+    # Не указаны действия, если продукта не существует, это приведет к ошибке в следующем шаге
 
     if product.available:
         # product.available = False лучше сделать сразу после проверки
@@ -20,10 +21,12 @@ def buy(cls, user, item_id):
         user.withdraw(product.price)
         # информация о купленном товаре
         # информация о товаре не отправляется а метод, только о пользователе
+        # такие действия можно выполнить в celery задачах, так как операция может быть долгая или
+        # может вызвать ошибку
         send_email_to_user_of_buy_product(user)
         product.available = False
         product.buyer = user
         product.save()
         return True
     else:
-return False
+        return False
